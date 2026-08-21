d488ae1545b78e3e9e59d6a2afe1d29c733c41e3
dev-con-001 <tvailc@yorksolutions.net>
2026-08-21 13:18:57 -0500

    Revert "BReaking ThE pRoGraM"
    This reverts commit 71589e23a3175a41b5e807ed7e30a695e270b11b.

--- main.py
+++ main.py
@@ -32,19 +32,19 @@ COMMANDS = {
     "summary": summary
 }
 
-#
-# def main(argv):
-#     command = argv[1] if len(argv) > 1 else "help"
-#     roster = {}
-#     try:
-#         COMMANDS[command](roster, argv[2:])
-#     except KeyError:
-#         print(f"unknown command: {command}", file=sys.stderr)
-#         return 1
-#     except GradebookError as e:
-#         print(f"error: {e}", file=sys.stderr)
-#         return 1
-#     return 0
+
+def main(argv):
+    command = argv[1] if len(argv) > 1 else "help"
+    roster = {}
+    try:
+        COMMANDS[command](roster, argv[2:])
+    except KeyError:
+        print(f"unknown command: {command}", file=sys.stderr)
+        return 1
+    except GradebookError as e:
+        print(f"error: {e}", file=sys.stderr)
+        return 1
+    return 0
 
 
 if __name__ == "__main__":
